ebbbd4dd62e8db1716d6cff7b9eab72a43d5cb06
Pavan-Kumar-Edara <114322806+Pavan-Kumar-Edara@users.noreply.github.com>
2023-04-07 18:14:49 +0530

    Add files via upload

---
+++ Day4-Question12/Grandfather.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day4_oops
+{
+    public class Grandfather
+    {
+        public int age;
+        public Grandfather(int age)
+        {
+            this.age = age;
+        }
+        public void DisplayGrandFather()
+        {
+            Console.WriteLine("Grandfather age is {0} times that of father.",age);
+        }
+    }
+    public class Father : Grandfather
+    {
+        public int period;
+        public Father(int age,int period):base(age) { 
+                this.period = period;
+        }
+        public void DisplayFather()
+        {
+            Console.WriteLine("Father was at the period of {0}  with an age of {1}.",period,age);
+        }
+    }
+    public class Son : Father
+    {
+        public Son(int age,int period):base (age,period) { }
+        public void DisplayChild()
+        {
+            Console.WriteLine("Son was at the period of {0}'s with an age of {1}.",period,age);
+        }
+    }
+}
---
+++ Day4-Question12/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Day4_oops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Grandfather grandfather = new Grandfather(4);
+            grandfather.DisplayGrandFather();
+
+            Son son = new Son(10, 20);
+            son.DisplayChild();
+            son.DisplayGrandFather();
+
+        }
+    }
+}
