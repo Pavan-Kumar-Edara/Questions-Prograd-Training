7a01106486faad330a86f5fd89020830f71651f4
Pavan-Kumar-Edara <114322806+Pavan-Kumar-Edara@users.noreply.github.com>
2023-04-07 17:41:47 +0530

    Add files via upload

---
+++ Day2-question6/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace loops_question
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a string");
+            string stringValue = Console.ReadLine();
+            string outputOne= "";
+            string outputTwo = " ";
+            for(int i = 0;i<stringValue.Length;i++)
+            {
+                 if (i%2 == 0) {
+                    outputOne += stringValue[i];
+                }
+                 else
+                {
+                    outputTwo += stringValue[i];
+                }
+            }
+            Console.WriteLine(outputOne + " " +  outputTwo);
+        }
+    }
+}
