97db1b26b6392710faeb765003cff0e856a1b94e
Pavan-Kumar-Edara <114322806+Pavan-Kumar-Edara@users.noreply.github.com>
2023-04-07 17:54:59 +0530

    Add files via upload

---
+++ Day3-Question8/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Question3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter string:");
+            string stringvalue=Console.ReadLine();
+
+            if (stringvalue.Length <=4)
+            {
+                Console.WriteLine(stringvalue);
+            }
+            else
+            {
+                string outputString = "";
+                for (int iteration = 0; iteration < stringvalue.Length - 4; iteration++)
+                {
+                    outputString += "#";
+                }
+                outputString += stringvalue.Substring(stringvalue.Length - 4);
+                Console.WriteLine(outputString);
+            }
+        
+        }
+    }
+}
