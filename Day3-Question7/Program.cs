9b3a46ad6d4361f57bcd6d4a69a912cc82d5de25
Pavan-Kumar-Edara <114322806+Pavan-Kumar-Edara@users.noreply.github.com>
2023-04-07 17:51:36 +0530

    Add files via upload

---
+++ Day3-Question7/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Question2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string outPut;
+            Console.WriteLine("Enter a noun:");
+            string noun=Console.ReadLine();
+
+            if (noun[0] == noun[noun.Length-1])
+            {
+                outPut= char.ToUpper(noun[0]) + noun.Substring(1) + noun.Substring(1);
+            }
+            else
+            {
+                outPut = "The " + char.ToUpper(noun[0]) + noun.Substring(1); ;
+            }
+
+            Console.WriteLine(outPut);
+        }
+    }
+}
