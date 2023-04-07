56d169032d32c19e363093b118a4118be8621061
Pavan-Kumar-Edara <114322806+Pavan-Kumar-Edara@users.noreply.github.com>
2023-04-07 18:08:49 +0530

    Add files via upload

---
+++ Day4-Question10/Block.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question
+{
+    public class Block
+    {
+        public int width;
+        public int length;
+        public int height;
+
+        public Block(int[] dimensions)
+        {
+            this.width = dimensions[0];
+            this.length = dimensions[1];
+            this.height = dimensions[2];
+        }
+
+        public int GetWidth()
+        {
+            return width;
+        }
+
+        public int GetLength()
+        {
+            return length;
+        }
+
+        public int GetHeight()
+        {
+            return height;
+        }
+
+        public int GetVolume()
+        {
+            return width * length * height;
+        }
+
+        public int GetSurfaceArea()
+        {
+            return 2 * (width * length + width * height + length * height);
+        }
+    }
+}
+
---
+++ Day4-Question10/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Question
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] dimensions = new int[3];
+            Console.WriteLine("Enter width,length,height:");
+            for (int iterator=0;iterator<dimensions.Length; iterator++)
+            {
+                dimensions[iterator] = int.Parse(Console.ReadLine());
+            }
+            
+            Block blockCode = new Block(dimensions);
+
+            Console.WriteLine("width: "+blockCode.GetWidth()); 
+            Console.WriteLine("length: "+blockCode.GetLength());  
+            Console.WriteLine("height: "+blockCode.GetHeight());  
+            Console.WriteLine("volume: "+blockCode.GetVolume());  
+            Console.WriteLine("surfacearea: "+blockCode.GetSurfaceArea());  
+        }
+    }
+}
