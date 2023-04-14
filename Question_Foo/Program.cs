d70503bbbb5832f6b0535770d6df18f55fa7d68b
Pavan-Kumar-Edara <114322806+Pavan-Kumar-Edara@users.noreply.github.com>
2023-04-14 18:46:18 +0530

    Add files via upload

---
+++ Question_Foo/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace Question
+{
+    public class foo
+    {
+     
+        public static void first() { 
+            Console.WriteLine("first");
+        }
+        public static void second() { 
+            Console.WriteLine("second"); 
+        }
+        public static void third() {
+            Console.WriteLine("third"); 
+        }
+        static void Main(string[] args)
+        {
+            
+            int count = 3;
+            List<int> list = new List<int>();
+            for(int iterator=0; iterator<count; iterator++)
+            {
+                Console.WriteLine($"Enter value {iterator}:");
+                list.Add(int.Parse(Console.ReadLine()));
+            }
+           
+
+            //foo instance = new foo();
+
+            Thread threadA = new Thread(first);
+            Thread threadB = new Thread(second);
+            Thread threadC = new Thread(third);
+
+            for(int iterator=0; iterator < count; iterator++)
+            {
+                if (list[iterator] == 1)
+                {
+                     threadA.Start();
+                }
+                else if (list[iterator] == 2)
+                {
+                    threadB.Start();
+                }
+                else
+                {
+                    threadC.Start();
+                }
+            }
+        }
+    }
+}
+
+
