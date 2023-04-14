dfd867597c12397a4ec6102d677786c073025108
Pavan-Kumar-Edara <114322806+Pavan-Kumar-Edara@users.noreply.github.com>
2023-04-14 18:49:31 +0530

    Add files via upload

---
+++ Removing stars/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Removing_stars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter string:");
+            string stringValue = Console.ReadLine();
+            Stack<char> stack = new Stack<char>();
+
+            foreach (var item in stringValue)
+            {
+
+                if (item != '*')
+                {
+                    stack.Push(item);
+                }
+                else
+                {
+                    if (stack.Count > 0)
+                    {
+                        stack.Pop();
+                    }
+                }
+            }
+
+            char[] charArray = stack.ToArray();
+
+            Array.Reverse(charArray);
+            Console.WriteLine(charArray);
+
+
+        }
+    }
+}
