9a5a005c2977703dc7b0e3e5e42941cf9199b94f
Pavan-Kumar-Edara <114322806+Pavan-Kumar-Edara@users.noreply.github.com>
2023-04-14 19:34:17 +0530

    Add files via upload

---
+++ Anangrams/Solution.cs
@@ -0,0 +1,27 @@
+﻿namespace Anangrams
+{
+    public  class Solution
+    {
+        public IList<IList<string>> GroupAnagrams(string[] strs)
+        {
+            var dict = new Dictionary<string, IList<string>>();
+
+            foreach (var s in strs)
+            {
+                var sorted = new string(s.OrderBy(c => c).ToArray());
+                if (!dict.ContainsKey(sorted))
+                    dict[sorted] = new List<string>();
+                dict[sorted].Add(s);
+            }
+
+            return dict.Values.ToList();
+        }
+        static void Main(string[] args)
+        {
+            var strs = new string[] { "eat", "tea", "tan", "ate", "nat", "bat" };
+            var solution = new Solution();
+            var result = solution.GroupAnagrams(strs);
+            Console.WriteLine(string.Join(", ", result.Select(l => $"[{string.Join(", ", l)}]")));
+        }
+    }
+ }
