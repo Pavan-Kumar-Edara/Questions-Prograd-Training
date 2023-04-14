52e948a6033042b6242f0b3d27a8fa3784174c71
Pavan-Kumar-Edara <114322806+Pavan-Kumar-Edara@users.noreply.github.com>
2023-04-14 19:46:46 +0530

    Add files via upload

---
+++ Qurak Reverse/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace week1_day5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Before Interact:");
+            Quark quarkObjOne=new Quark("red","up");
+            quarkObjOne.DisplayColorFlavor();
+
+            Quark quarkObjTwo = new Quark("blue", "strange");
+            quarkObjTwo.DisplayColorFlavor();
+            Console.WriteLine();
+
+            Console.WriteLine("After Interact");
+            quarkObjOne.Interact(quarkObjTwo);
+            quarkObjOne.DisplayColorFlavor();
+            quarkObjTwo.DisplayColorFlavor();
+
+        }
+    }
+}
---
+++ Qurak Reverse/Quark.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week1_day5
+{
+    public class Quark
+    {
+        public string color;
+        public string flavor;
+        public double baryonNumber;
+        
+        public Quark(string color,string flavor)
+        {
+            this.color = color;
+            this.flavor = flavor;
+            this.baryonNumber = 1.0/3.0;
+        }
+
+        public void DisplayColorFlavor()
+        {
+            Console.WriteLine(color+","+flavor+","+baryonNumber);
+        }
+        public void Interact(Quark otherQuark)
+        {
+                string tempColor=this.color;
+                this.color=otherQuark.color;
+                otherQuark.color=tempColor;
+        }
+
+
+    }
+}
