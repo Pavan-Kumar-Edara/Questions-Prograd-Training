fd6f09522f46a027d816e4a71e29e616bbdf0125
Pavan-Kumar-Edara <114322806+Pavan-Kumar-Edara@users.noreply.github.com>
2023-04-14 18:56:30 +0530

    Add files via upload

---
+++ Partial-Methods/Partial.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Partial_Methods
+{
+    public partial class Partial
+    {
+        public void Display() {
+            Console.WriteLine("Partial Methods.");
+        }
+        public partial void Add(int numOne, int numTwo);
+
+       
+    }
+    public partial class Partial
+    {
+        public void Addition(int numOne,int numTwo)
+        {
+            Add(numOne,numTwo);
+        }
+        public partial void Add(int numOne,int numTwo)
+        {
+            int result = numOne + numTwo;
+            Console.WriteLine("Addition of two numbers is:"+result);
+        }
+    }
+}
---
+++ Partial-Methods/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Partial_Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Partial partialobj = new Partial();
+            partialobj.Addition(10,50);
+        }
+    }
+}
