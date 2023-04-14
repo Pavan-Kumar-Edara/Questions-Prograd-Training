f470332bf688ce798b64844849177f3cd10b9824
Pavan-Kumar-Edara <114322806+Pavan-Kumar-Edara@users.noreply.github.com>
2023-04-14 18:54:54 +0530

    Add files via upload

---
+++ partial classes/PartialClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace partial_classes
+{
+    public partial class PartialClass
+    {
+        
+        public string employeeName { get; set; }
+        public int employeeId { get; set; }
+
+        public partial void Display();
+    }
+
+    public partial class PartialClass
+    {
+        public partial void Display()
+        {
+            Console.WriteLine("Name is:"+employeeName);
+            Console.WriteLine("Id is "+employeeId);
+        }
+    }
+}
---
+++ partial classes/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace partial_classes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PartialClass employeeOne = new PartialClass();
+            employeeOne.employeeName = "John";
+            employeeOne.employeeId = 5;
+            employeeOne.Display();
+
+            PartialClass employeeTwo = new PartialClass();
+            employeeTwo.employeeName = "Ro";
+            employeeTwo.employeeId = 2;
+            employeeTwo.Display();
+            
+        }
+    }
+}
