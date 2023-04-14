b5aef249488a195c68b5faca9b504f0e0ddac335
Pavan-Kumar-Edara <114322806+Pavan-Kumar-Edara@users.noreply.github.com>
2023-04-14 18:37:28 +0530

    Add files via upload

---
+++ Generics classes and Methods/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Generics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GenericClass<string> name = new GenericClass<string>();
+            name.GenericMethod<string>("Generic Class.");
+
+            GenericClass<int> count = new GenericClass<int>();
+            count.GenericMethod<int>(1);
+
+            GenericClass<char> firstLetter = new GenericClass<char>();
+            firstLetter.GenericMethod('G');
+        }
+    }
+}
