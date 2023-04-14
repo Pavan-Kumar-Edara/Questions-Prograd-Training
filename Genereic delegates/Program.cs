78327403d609bb7f7d0a996cf4ae5ebf01cac8f4
Pavan-Kumar-Edara <114322806+Pavan-Kumar-Edara@users.noreply.github.com>
2023-04-14 18:53:19 +0530

    Add files via upload

---
+++ Genereic delegates/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Genereic_delegates
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Func<int, int, int> add = (valueOne, valueTwo) =>
+            {
+                return valueOne + valueTwo;
+            };
+            int sum = add(10, 20);
+            Console.WriteLine("Func delegate: " + sum);
+
+
+            Action<int, double> againAdd = (valueOne, valueTwo) =>
+            {
+                var result = valueOne + valueTwo;
+                Console.WriteLine("Action delegate:"+result);
+            };
+            againAdd(30, 20.5);
+
+            Predicate<int> isEven = (x) => x % 2 == 0;
+            bool result = isEven(10);
+            Console.WriteLine("Predicate delegate: " + result);
+
+        }
+    }
+}
