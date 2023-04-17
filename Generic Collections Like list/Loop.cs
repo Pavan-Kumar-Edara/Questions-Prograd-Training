b71792a7b8b5554a02fb7a041b34f1f621a72986
Pavan-Kumar-Edara <114322806+Pavan-Kumar-Edara@users.noreply.github.com>
2023-04-17 23:45:16 +0530

    Add files via upload

---
+++ Generic Collections Like list/Loop.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic_Collections_Like_list
+{
+    public class Loop<T>
+    {
+        public void RotateLeft(int[] arr, int size)
+        {
+            for (int iterator = 0; iterator < 2; iterator++)
+            {
+                int temp = arr[0];
+
+                for (int iteratorTwo = 0; iteratorTwo < arr.Length - 1; iteratorTwo++)
+                {
+                    arr[iteratorTwo] = arr[iteratorTwo + 1];
+                }
+
+                arr[arr.Length - 1] = temp;
+            }
+
+        }
+        public void RotateRight(int[] arr, int size)
+        {
+            for (int iterator = 0; iterator < 1; iterator++)
+            {
+                int lastElement = arr[arr.Length - 1];
+                for (int iteratorTwo = arr.Length - 1; iteratorTwo > 0; iteratorTwo--)
+                {
+                    arr[iteratorTwo] = arr[iteratorTwo - 1];
+                }
+                arr[0] = lastElement;
+            }
+
+        }
+        public void display(int[] arr)
+        {
+            for (int iterator = 0; iterator < arr.Length; iterator++)
+            {
+                Console.WriteLine(arr[iterator]);
+            }
+        }
+
+        public void Showfirst(int[] arr)
+        {
+            Console.WriteLine(arr[0]);
+        }
+        public void PopFirst(int[] arr)
+        {
+            Console.WriteLine(arr[0]);
+            for (int iterator = 1; iterator < arr.Length - 1; iterator++)
+            {
+                arr[iterator] = arr[iterator + 1];
+            }
+            arr[0] = default(int);
+
+        }
+    }
+}
---
+++ Generic Collections Like list/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Generic_Collections_Like_list
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the size of the array:");
+            int size = int.Parse(Console.ReadLine());
+            int[] array = new int[size];
+            Console.WriteLine("Enter the Elements of the array:");
+            for (int iterator = 0; iterator < size; iterator++)
+            {
+                array[iterator] = int.Parse(Console.ReadLine());
+            }
+
+            Loop<int> loop = new Loop<int>();
+            Console.WriteLine("To Rotate right:");
+
+            loop.RotateRight(array, size);
+            loop.display(array);
+
+            Console.WriteLine("To Rotate left:");
+            loop.RotateLeft(array, size);
+
+            loop.display(array);
+
+            Console.WriteLine("To show first:");
+            loop.Showfirst(array);
+
+            Console.WriteLine("To pop first:");
+            loop.PopFirst(array);
+
+
+        }
+    }
+    
+}
+
