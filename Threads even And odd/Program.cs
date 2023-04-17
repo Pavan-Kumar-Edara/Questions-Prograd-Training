48c46dbbca11e9d2629b0471da37c7c4748f7df9
Pavan-Kumar-Edara <114322806+Pavan-Kumar-Edara@users.noreply.github.com>
2023-04-17 23:31:58 +0530

    Add files via upload

---
+++ Threads even And odd/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Threads
+{
+    public class Program
+    {
+        public static void Zero(object zero)
+        {
+            Console.WriteLine(zero);
+        }
+        public static void Even(object element)
+        {
+            Console.WriteLine(element);
+        }
+        public static void Odd(object element)
+        {
+            Console.WriteLine(element);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the Number:");
+            int num = Convert.ToInt32(Console.ReadLine());
+            Thread ThreadA = new Thread(Zero);
+            Thread ThreadB = new Thread(Even);
+            Thread ThreadC = new Thread(Odd);
+            ThreadA.Start();
+            ThreadB.Start();
+            ThreadC.Start();
+            int zero = 0;
+            for (int element = 1; element <= num; element++)
+            {
+                Zero(zero);
+                if (element % 2 == 0)
+                {
+                    Even(element);
+                }
+                else
+                {
+                    Odd(element);
+                }
+            }
+        }
+    }
+}
